197cc3dcb1a54eeb0698a9ad4fa7166cd8d42c10
raul <raul@losmaquiladores.com>
2013-07-02 13:01:24 -0500

    redes en cada imagen, navegación "automatica", estilos

--- js/functions.js
+++ js/functions.js
@@ -25,8 +25,40 @@ $(function(){
 		scroll = $(window).scrollTop();
 		//Nav
 		nav.animate({
-			'right': 100
+			'right': 30
 		}, 500);
+
+		var donde_estoy_auto = $(window).scrollTop();
+		var dias = $('.dia');
+
+		if ( donde_estoy_auto < 600 ){
+			dias.removeClass('hovered');
+
+		} else if ( donde_estoy_auto >= 600 && donde_estoy_auto < 1500  ){
+			dias.removeClass('hovered');
+			$('.uno').addClass('hovered');
+		} else if ( donde_estoy_auto >= 1500 && donde_estoy_auto < 2400  ){
+			dias.removeClass('hovered');
+			$('.dos').addClass('hovered');
+		} else if ( donde_estoy_auto >= 2400 && donde_estoy_auto < 3300  ){
+			dias.removeClass('hovered');
+			$('.tres').addClass('hovered');
+		} else if ( donde_estoy_auto >= 3300 && donde_estoy_auto < 4200  ){
+			dias.removeClass('hovered');
+			$('.cuatro').addClass('hovered');
+		} else if ( donde_estoy_auto >= 4200 && donde_estoy_auto < 5100  ){
+			dias.removeClass('hovered');
+			$('.cinco').addClass('hovered');
+		} else if ( donde_estoy_auto >= 5100 && donde_estoy_auto < 6000  ){
+			dias.removeClass('hovered');
+			$('.seis').addClass('hovered');
+		} else if ( donde_estoy_auto >= 6000 && donde_estoy_auto < 6900  ){
+			dias.removeClass('hovered');
+			$('.siete').addClass('hovered');
+		} else if ( donde_estoy_auto >= 6900 && donde_estoy_auto < 7800  ){
+			dias.removeClass('hovered');
+			$('.ocho').addClass('hovered');
+		}
 
 	});
 
@@ -74,26 +106,9 @@ $(function(){
 
 		if ( $(this).hasClass('up') ){
 			navegacion_up();
-
-
 		} else if ( $(this).hasClass('down') ){
 			navegacion_down();
-
 		}
-	});
-
-	//Efecto share
-	var compartir = $('.compartir'),
-		fb = $('.facebook'),
-		twitter = $('.twitter'),
-		pinterest = $('.pinterest'),
-		mail = $('.mail'),
-		todos_compartir = $('.facebook, .twitter, .pinterest, .mail');
-
-	todos_compartir.hide();
-
-	compartir.on('click', function(){
-		todos_compartir.slideToggle('fast');
 	});
 
 	$(window).on('keydown', function(e){
@@ -119,100 +134,6 @@ $(function(){
 		mail_str = "mailto:?subject=";
 		mail_str += "&body=" + content ;
 		location.href = mail_str;
-	});
-
-	//Pinterest
-
-	$('#pinterest a').on('click', function(){
-		var href = '//pinterest.com/pin/create/button/',
-		url = 'http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F',
-		media = encodeURIComponent( contenido_share() ),
-		description = encodeURIComponent('Éste es uno de los consejos para disfrutar de una lactancia exitosa junto a tu bebé. Amamanta, hazlo por los dos'),
-		pinterest_url = href + '?url=' + url + '&media=' + media + '&description=' + description;
-
-		$(this).attr('href', pinterest_url );
-	});
-
-	//Facebook
-	$('#facebook').on('click', function(){
-
-		donde_estoy = $(window).scrollTop();
-
-		var link,
-			picture,
-			name,
-			caption;
-
-		if ( donde_estoy < 600 ){
-
-
-		} else if ( donde_estoy >= 600 && donde_estoy < 1500 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip1.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 1') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 1') + '&';
-
-		} else if ( donde_estoy >= 1500 && donde_estoy < 2400 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip2.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 2') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 2') + '&';
-
-		} else if ( donde_estoy >= 2400 && donde_estoy < 3300 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip3.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 3') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 3') + '&';
-
-		} else if ( donde_estoy >= 3300 && donde_estoy < 4200 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip4.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 4') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 4') + '&';
-
-		} else if ( donde_estoy >= 4200 && donde_estoy < 5100 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip5.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 5') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 5') + '&';
-
-		} else if ( donde_estoy >= 5100 && donde_estoy < 6000 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip6.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 6') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 6') + '&';
-
-		} else if ( donde_estoy >= 6000 && donde_estoy < 6900 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip7.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 7') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 7') + '&';
-
-		} else if ( donde_estoy >= 6900 && donde_estoy < 7800 ){
-
-			link = 'link=http://publicislead.com/lactancia/#uno&';
-			picture = 'picture=http://publicislead.com/lactancia/share_img/tip1.jpg&';
-			name = 'name=' + encodeURIComponent('Tip 1') + '&';
-			caption = 'caption=' + encodeURIComponent('Tip 1') + '&';
-
-		}
-
-		var href = 'https://www.facebook.com/dialog/feed?';
-		var app_id = 'app_id=593365504028765&';
-		var description = 'description=' + encodeURIComponent('Amamantar es un esfuerzo que vale la pena. Éste es uno de los prácticos consejos que te ayudarán a disfrutar de una lactancia exitosa junto a tu bebé. Amamanta, hazlo por los dos.') + '&';
-		var redirect_uri = 'redirect_uri=http://publicislead.com/lactancia';
-
-		$('#facebook a').attr('href', href + app_id + picture + name + caption + description + redirect_uri );
-
-		location.href = href + app_id + picture + name + caption + description + redirect_uri;
-
 	});
 
 	function navegacion_up(){
